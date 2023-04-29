5d03ad966d2046a44ba069588dfdf84f24aa7fdd
Ab-verse <abhisheksckm17@gmail.com>
2023-04-30 01:56:07 +0530

    test tailwind

--- client/thirdweb-app/src/App.jsx
+++ client/thirdweb-app/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <p className="font-xl font-bold">TEST</p>
+    </div>
+  );
 };
 
 export default App;
