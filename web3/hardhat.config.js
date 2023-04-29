ce777536873dcfa87dc3babf3d58d559739619e9
Ab-verse <abhisheksckm17@gmail.com>
2023-04-29 14:23:44 +0530

    write crowd-funding-smart-contract

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,7 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: process.env.GOERLI_RPC_URL,
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
