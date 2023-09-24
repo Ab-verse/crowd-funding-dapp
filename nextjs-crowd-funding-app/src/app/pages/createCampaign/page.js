94f9708a1c4a509b2d7c6e94b99cf1b80d943b5f
Ab-verse <abhisheksckm17@gmail.com>
2023-09-24 22:00:11 +0530

    add rainbow-kit

---
+++ nextjs-crowd-funding-app/src/app/pages/createCampaign/page.js
@@ -0,0 +1,18 @@
+"use client"
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <style jsx global>{`
+        body {
+          background: ${"blue"};
+        }
+      `}</style>
+      <div>Home</div>
+    </>
+  );
+};
+
+export default Home;
